633d626377b6e34f88758838b05ed8844532d9d3
MohammedOttun <ottunmo@mac.com>
2021-08-10 19:03:37 -0500

    Login/Signup and authentication.

---
+++ server/utils/auth.js
@@ -0,0 +1,11 @@
+const jwt = require('jsonwebtoken');
+
+const secret = 'guilttrip$3cr3t';
+const expiration = '2h';
+
+module.exports = {
+  signToken: function ({ email, name, _id }) {
+    const payload = { email, name, _id };
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};
