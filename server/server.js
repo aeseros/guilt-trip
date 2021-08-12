a03ffcf89710e5c9812293674dd0de7dd85de0d8
Anna Uyen K.P <79038087+a-ukp@users.noreply.github.com>
2021-08-12 15:22:33 -0500

    reconnect to cloud mongo

--- server/server.js
+++ server/server.js
@@ -7,40 +7,6 @@ const  { typeDefs, resolvers } = require('./schemas');
 
 dotenv.config();
 
-// async function startServer() {
-//     const PORT = process.env.PORT || 5000;
-//     const app = express();
-//     const apolloServer = new ApolloServer({
-//         typeDefs,
-//         resolvers,
-//     });
-
-//     await apolloServer.start();
-
-//     apolloServer.applyMiddleware({ app });
-//     app.use(express.urlencoded({ extended: true }));
-//     app.use(express.json());
-
-//     // if (process.env.NODE_ENV === 'production') {
-//     //     app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
-
-//     app.use((req, res) => {
-//         res.send('/')
-//     });
-
-//     // app.get('*', (req, res) => {
-//     //     res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'))
-//     // })
-
-//     db.once('open', () => {
-//         app.listen(PORT, () => {
-//             console.log(`listening on http://localhost:${PORT}/`)
-//         })
-//     });
-// };
-
-// startServer();
-
 async function startServer() {
 
     const app = express();
@@ -57,8 +23,7 @@ async function startServer() {
     app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
 
-    // if we're in production, serve client/build as static assets
-    if (process.env.NODE_ENV === 'production') {
+    if (process.env.NODE_ENV == 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
     }
 
@@ -67,14 +32,12 @@ async function startServer() {
     });
 
     db.once('open', () => {
-    app.listen(PORT, () => {
-        console.log(`API server running on port ${PORT}!`);
-        // log where we can go to test our GQL API
-        console.log(`Use GraphQL at http://localhost:${PORT}${apolloServer.graphqlPath}`);
-    });
+        app.listen(PORT, () => {
+            console.log(`API server running on port ${PORT}!`);
+            console.log(`Use GraphQL at http://localhost:${PORT}${apolloServer.graphqlPath}`);
+        });
     });
 
 };
 
 startServer();
-
